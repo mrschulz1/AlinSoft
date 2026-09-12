f9891b2e4a8dcb2a6cd1af362651dc34f414c2f2
victor <mrschulz@live.cl>
2026-09-12 02:20:15 -0300

    ordeen con fotos

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,19 @@
-const SUPABASE_URL = 'https://okkydrwdexacxvfbtopb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ra3lkcndkZXhhY3h2ZmJ0b3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMjU0MzgsImV4cCI6MjA5OTcwMTQzOH0.ekbFeG6r176fbanPDUin0O_jwDcnmRRUVrJL3ohgXeE';
+
+
+// Variable global para almacenar la instancia única
+let supabaseInstance = null;
 
 function initSupabase() {
-    return supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    if (!supabaseInstance) {
+        const SUPABASE_URL = 'https://okkydrwdexacxvfbtopb.supabase.co';
+        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ra3lkcndkZXhhY3h2ZmJ0b3BiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMjU0MzgsImV4cCI6MjA5OTcwMTQzOH0.ekbFeG6r176fbanPDUin0O_jwDcnmRRUVrJL3ohgXeE'; // Asegúrate de colocar tu llave pública real
+
+        if (typeof supabase !== 'undefined') {
+            supabaseInstance = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+        } else {
+            console.error("El SDK de Supabase no se ha cargado.");
+            return null;
+        }
+    }
+    return supabaseInstance;
 }
